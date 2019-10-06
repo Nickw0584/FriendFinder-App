5f24ff131c0405ad759659ef5067909b7f9207ef
Nick Wilson <nickw0584@icloud.com>
2019-10-06 17:28:17 -0500

    check

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./app/routing/htmlRoutes")(app);
-require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
 
 
 
