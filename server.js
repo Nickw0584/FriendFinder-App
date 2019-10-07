b543649e21d22ad83acc68faf8248dcc7455f5cd
Nick Wilson <nickw0584@icloud.com>
2019-10-07 15:08:56 -0500

    changed port to PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.port || 8080;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 
